17e32ebd57be3500247c2958850caf7a7fd70c2c
aossucci <antonio.ossucci@gmail.com>
2022-10-26 16:58:54 -0300

    Criado rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-const express = require('express')
+import * as express from 'express'
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World 2')
+  res.send('GET2 /')
+})
+
+app.post('/', function (req, res) {
+  res.send('POST /')
+})
+
+app.put('/', function (req, res) {
+  res.send('PUT /')
+})
+
+app.delete('/', function (req, res) {
+  res.send('DELETE /')
+})
+
+app.patch('/', function (req, res) {
+  res.send('PATCH /')
 })
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
